ac7adef25a129b48d8c69979e8cd9deaae366883
Maria Pankratova <mirraim@gmail.com>
2020-09-22 22:49:57 +0500

    add method "create" for Contract, change constructor, deleted field PREFIX

--- src/main/java/ru/mirraim/Personal/documents/Contract.java
+++ src/main/java/ru/mirraim/Personal/documents/Contract.java
@@ -1,13 +1,22 @@
 package ru.mirraim.Personal.documents;
 
 import ru.mirraim.Personal.Employee;
+import ru.mirraim.Personal.Staff;
 
 public class Contract extends Document{
-    private static final String PREFIX = "CON-";
     private String position;
 
     public Contract(Employee employee) {
         super(employee);
         this.position = employee.getPosition().toString().toLowerCase();
+        setPrefix("CON-");
     }
+
+    public static Contract createContract(Employee employee){
+        Contract contract = new Contract(employee);
+        contract.setDocumentNumber(Staff.getContracts().size() + 1);
+        return contract;
+    }
+
+
 }
